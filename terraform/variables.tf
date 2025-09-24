373fbb853fe74624dbe100e063ffeed30850b690
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 06:03:19 +0000

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
-resource "aws_ecr_repository" "strapi" {
-  name = "${var.app_name}-repo"
+variable "key_pair" {
+  description = "EC2 Key Pair name for SSH access"
+  type        = string
+  default     = "strapi-mayank"  # Replace with your actual key pair name if different
 }
