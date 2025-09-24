50110cd116b79bf7c98ea0658d1012e5819e07d4
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 06:59:17 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,19 +1,32 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_ecs_cluster" "strapi_cluster" {
+  name = "strapi-cluster"
+}
+
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  network_mode             = "awsvpc"
 
-  container_definitions = jsonencode([{
-    name      = "strapi"
-    image     = var.docker_image  # pass existing ECR image as variable
-    essential = true
-    portMappings = [{
-      containerPort = 1337
-      hostPort      = 1337
-    }]
-  }])
+  network_mode = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = var.docker_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+        }
+      ]
+    }
+  ])
 }
 
 resource "aws_ecs_service" "strapi_service" {
@@ -21,12 +34,13 @@ resource "aws_ecs_service" "strapi_service" {
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
   launch_type     = "FARGATE"
+  desired_count   = 1
 
   network_configuration {
-    subnets         = [aws_subnet.strapi_subnet_1.id, aws_subnet.strapi_subnet_2.id]
-    security_groups = [aws_security_group.strapi_sg.id]
+    subnets         = var.subnet_ids
+    security_groups = [var.security_group_id]
     assign_public_ip = true
   }
 
-  desired_count = 1
+  depends_on = [aws_ecs_task_definition.strapi_task]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,27 @@
 variable "docker_image" {
-  description = "The ECR image URI for Strapi"
+  description = "ECR image for Strapi"
   type        = string
 }
 
-variable "vpc_id" {
-  description = "VPC ID to deploy ECS in"
+variable "key_pair" {
+  description = "EC2 Key Pair name for SSH (if needed)"
   type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs for Fargate service"
-  type        = list(string)
 }
 
 variable "security_group_id" {
-  description = "Security group ID for ECS service"
+  description = "Existing Security Group ID for ECS service"
   type        = string
+  default     = "sg-0fc419b83ffd83dd6"
+}
+
+variable "vpc_id" {
+  description = "VPC ID for ECS tasks"
+  type        = string
+  default     = "vpc-098cc44dc4ec933d7"
+}
+
+variable "subnet_ids" {
+  description = "List of Subnet IDs for ECS tasks"
+  type        = list(string)
+  default     = ["subnet-08f7b9dfa8bd5c307"]
 }
