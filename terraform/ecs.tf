3e025e0efb0f4dee49e7f2099a09ca2d20f1f389
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 09:18:30 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -36,6 +36,29 @@ resource "aws_ecs_service" "strapi_service" {
     assign_public_ip = true
   }
 
+variable "ecs_execution_role_arn" {}
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = [var.security_group_id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  execution_role_arn = var.ecs_execution_role_arn
+}
+
   load_balancer {
     target_group_arn = aws_lb_target_group.strapi_tg.arn
     container_name   = "strapi"
--- terraform/iam.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role_policy_attachment" "ecs_exec_ecr" {
-  role       = "strapi-ecs-execution-role" # <-- name of your existing role
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
