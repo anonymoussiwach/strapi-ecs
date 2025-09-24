4d174a944e03902a7703d19529b3061442e67f42
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 07:59:32 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -36,11 +36,3 @@ resource "aws_ecs_service" "strapi_service" {
     assign_public_ip = true
   }
 }
-
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.strapi_cluster.id
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.strapi_service.name
-}
