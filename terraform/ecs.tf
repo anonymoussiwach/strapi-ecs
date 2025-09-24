6bd998040527237a7acb4081aec2538d44dd6693
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 10:47:47 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,99 +1,16 @@
-resource "aws_iam_role" "strapi_ecs_execution_role" {
-  name = "strapi-ecs-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "ecs-tasks.amazonaws.com" },
-      Action   = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.strapi_ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg-mayank"
-  description = "Allow HTTP access to Strapi"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_lb" "strapi_alb" {
-  name               = "strapi-alb-mayank"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.strapi_sg.id]
-  subnets            = data.aws_subnets.default.ids
-}
-
-resource "aws_lb_target_group" "strapi_tg" {
-  name        = "strapi-tg-mayank"
-  port        = 1337
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.default.id
-  target_type = "ip"
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener" "strapi_listener" {
-  load_balancer_arn = aws_lb.strapi_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.strapi_tg.arn
-  }
-}
+# ECS Cluster
 resource "aws_ecs_cluster" "strapi_cluster" {
   name = "strapi-cluster-mayank"
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn = aws_iam_role.strapi_ecs_execution_role.arn
+  execution_role_arn       = aws_iam_role.strapi_ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name      = "strapi"
@@ -108,6 +25,7 @@ resource "aws_ecs_task_definition" "strapi_task" {
   }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi_cluster.id
@@ -116,10 +34,11 @@ resource "aws_ecs_service" "strapi_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-  subnets         = var.subnet_ids
-  security_groups = [aws_security_group.strapi_sg.id]
-  assign_public_ip = true
-}
+    subnets         = var.subnet_ids
+    security_groups = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.strapi_tg.arn
     container_name   = "strapi"
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,3 +1,22 @@
+# ECS Execution Role
+resource "aws_iam_role" "strapi_ecs_execution_role" {
+  name = "strapi-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action   = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach managed policy for ECS to pull images from ECR
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.strapi_ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg-mayank"  # you can append random suffix if needed
   description = "Allow HTTP access to Strapi"
