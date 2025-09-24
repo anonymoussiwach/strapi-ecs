f07fbce482112aa03d72f4226795e037e4e2d119
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 14:24:35 +0000

    deploy

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -10,8 +10,8 @@ resource "aws_ecs_task_definition" "strapi_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn = aws_iam_role.ec2_ecr_full_access_role.arn
-  task_role_arn      = aws_iam_role.ec2_ecr_full_access_role.arn
+  execution_role_arn = "arn:aws:iam::145065858967:role/ec2_ecr_full_access_role"
+  task_role_arn      = "arn:aws:iam::145065858967:role/ec2_ecr_full_access_role"
 
   container_definitions = jsonencode([{
     name      = "strapi"
