6fca86c1b5550b455796615ecc1bfcf7c552b934
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 09:25:52 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -31,20 +31,6 @@ resource "aws_ecs_service" "strapi_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = data.aws_subnets.default.ids
-    security_groups = [aws_security_group.strapi_sg.id]
-    assign_public_ip = true
-  }
-
-variable "ecs_execution_role_arn" {}
-resource "aws_ecs_service" "strapi_service" {
-  name            = "strapi-service"
-  cluster         = aws_ecs_cluster.strapi_cluster.id
-  task_definition = aws_ecs_task_definition.strapi_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
     subnets         = var.subnet_ids
     security_groups = [var.security_group_id]
     assign_public_ip = true
@@ -57,13 +43,6 @@ resource "aws_ecs_service" "strapi_service" {
   }
 
   execution_role_arn = var.ecs_execution_role_arn
-}
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.strapi_tg.arn
-    container_name   = "strapi"
-    container_port   = 1337
-  }
 
   depends_on = [aws_lb_listener.strapi_listener]
 }
