6346ece9d15187bdd2e8ac6bfb0b4ead501601d6
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 07:06:31 +0000

    .

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,18 +1,51 @@
-provider "aws" {
-  region = "ap-south-1"
+# ecs.tf
+
+variable "docker_image" {}
+variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "security_group_id" {}
+variable "ecs_cluster_name" {
+  default = "strapi-cluster"
 }
 
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "strapi-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Cluster
 resource "aws_ecs_cluster" "strapi_cluster" {
-  name = "strapi-cluster"
+  name = var.ecs_cluster_name
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "256"
+  memory                   = "512"
 
-  network_mode = "awsvpc"
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -29,12 +62,13 @@ resource "aws_ecs_task_definition" "strapi_task" {
   ])
 }
 
+# ECS Service
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
-  launch_type     = "FARGATE"
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
     subnets         = var.subnet_ids
@@ -42,5 +76,19 @@ resource "aws_ecs_service" "strapi_service" {
     assign_public_ip = true
   }
 
-  depends_on = [aws_ecs_task_definition.strapi_task]
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_execution_role_policy
+  ]
+}
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.strapi_cluster.id
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.strapi_service.name
+}
+
+output "security_group_id" {
+  value = var.security_group_id
 }
