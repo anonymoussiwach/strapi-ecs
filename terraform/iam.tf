da9b9afde7cded55e51d1da4eef94df78c767684
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 09:11:10 +0000

    .

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role_policy_attachment" "ecs_exec_ecr" {
-  role       = aws_iam_role.strapi_ecs_execution_role.name
+  role       = "strapi-ecs-execution-role" # <-- name of your existing role
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
