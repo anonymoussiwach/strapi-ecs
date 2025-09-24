8f05093c37008df833c92eb62025431ef9b25a22
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 06:50:37 +0000

    .

---
+++ terraform/security.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Security group for Strapi ECS service"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "security_group_id" {
+  value = aws_security_group.strapi_sg.id
+}
