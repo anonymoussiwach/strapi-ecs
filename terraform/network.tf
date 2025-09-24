3ee59a901ab3d153cc5020e7188dc0c8df1f0fc6
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 07:02:00 +0000

    .

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,22 +16,3 @@ resource "aws_subnet" "strapi_subnet_2" {
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
-
-resource "aws_security_group" "strapi_sg" {
-  name   = "strapi-sg"
-  vpc_id = aws_vpc.strapi_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
