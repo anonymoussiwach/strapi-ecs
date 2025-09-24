e0bea58495d72877f969ea7e97aab1df780b57bf
Mayank Siwach <maxsiwach@gmail.com>
2025-09-24 11:15:15 +0000

    .

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,23 +1,3 @@
-resource "aws_ecs_task_definition" "strapi_task" {
-  family                   = "strapi-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = "arn:aws:iam::145065858967:role/ec2_ecr_full_access_role"  # existing role
-
-  container_definitions = jsonencode([{
-    name      = "strapi"
-    image     = var.docker_image
-    cpu       = 512
-    memory    = 1024
-    essential = true
-    portMappings = [{
-      containerPort = 1337
-      hostPort      = 1337
-    }]
-  }])
-}
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg-mayank"  # you can append random suffix if needed
   description = "Allow HTTP access to Strapi"
