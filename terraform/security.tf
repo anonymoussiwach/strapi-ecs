35e84a323bbcee4e220f6c58315834337c8358ed
Mayank Siwach <maxsiwach@gmail.com>
2025-09-25 05:36:01 +0000

    deploy

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,14 +1,35 @@
 # ALB Security Group
-resource "aws_security_group" "strapi_sg" {
+resource "aws_security_group" "alb_sg" {
   name        = "strapi-alb-sg"
-  description = "Allow HTTP access from the internet"
-  vpc_id      = "vpc-01b35def73b166fdc"
+  description = "Allow HTTP access from the internet to ALB"
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Public access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Task Security Group
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-ecs-sg"
+  description = "Allow ALB to access Strapi on 1337"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 1337
+    to_port         = 1337
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id] # Only ALB can reach ECS
   }
 
   egress {
