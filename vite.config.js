63dd9cb188b89b6242e957552716fb6ddc3e5bd3
Mayank Siwach <maxsiwach@gmail.com>
2025-09-25 05:54:58 +0000

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      'strapi-alb-mayank-1012566506.ap-south-1.elb.amazonaws.com'
-    ]
+    allowedHosts: [process.env.VITE_ALLOWED_HOSTS || 'localhost']
   }
-});
+})
