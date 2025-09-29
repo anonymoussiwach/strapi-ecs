b777a76bcf05a15b984d5bc306b9ac1cfdeb5379
Mayank Siwach <maxsiwach@gmail.com>
2025-09-29 12:49:18 +0000

    deploy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,10 +9,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # change this
+    bucket         = "mayank-terraform-bucket"   # change this
     key            = "strapi/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"             # optional but recommended
     encrypt        = true
   }
 }
