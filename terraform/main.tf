4c5b2161efc2fae5031cdc0448efc3315e31d0f7
Mayank Siwach <maxsiwach@gmail.com>
2025-10-03 07:33:34 +0000

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,13 +57,6 @@ resource "aws_ecs_service" "strapi_service" {
     security_groups  = [aws_security_group.strapi_sg.id]
     assign_public_ip = true
   }
-
-  # Register ECS tasks behind the ALB Target Group (required for Blue/Green)
-  load_balancer {
-    target_group_arn = aws_lb_target_group.strapi_tg.arn
-    container_name   = "strapi"
-    container_port   = 1337
-  }
-
+  
   depends_on = [aws_lb_listener.strapi_listener]
 }
