2ffc281c7bf580376a306f4f122ac625ef087d0e
Mayank Siwach <maxsiwach@gmail.com>
2025-10-04 05:15:06 +0000

    deploy

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -1,4 +1,43 @@
-ecs_service {
-  cluster_name = aws_ecs_cluster.strapi_cluster.name
-  service_name = aws_ecs_service.strapi_service.name
+resource "aws_codedeploy_app" "strapi_app" {
+  name             = "strapi-app-mayank"
+  compute_platform = "ECS"
+}
+
+resource "aws_codedeploy_deployment_group" "strapi_deploy_group" {
+  app_name              = aws_codedeploy_app.strapi_app.name
+  deployment_group_name = "strapi-deploy-group-mayank"
+  service_role_arn      = var.codedeploy_role_arn
+
+  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    terminate_blue_instances_on_deployment_success {
+      action = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.strapi_cluster.name
+    service_name = aws_ecs_service.strapi_service.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.strapi_listener.arn]
+      }
+      target_group {
+        name = aws_lb_target_group.strapi_tg_blue.name
+      }
+      target_group {
+        name = aws_lb_target_group.strapi_tg_green.name
+      }
+    }
+  }
 }
