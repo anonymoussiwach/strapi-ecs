dbb178608fbd026f18913d3ac924ef2e94fc33ee
Mayank Siwach <maxsiwach@gmail.com>
2025-10-04 04:42:43 +0000

    deploy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,21 +1,4 @@
-# IAM Role for CodeDeploy ECS
+# Reference the existing role (do not recreate it)
 data "aws_iam_role" "codedeploy_role" {
   name = "codedeploy-role-mayank"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-# Attach AWS Managed Policy
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
