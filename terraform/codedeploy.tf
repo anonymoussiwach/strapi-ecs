ea1b3a79fde98076feb0ed5d2f2fe05c48f2ad0d
Mayank Siwach <maxsiwach@gmail.com>
2025-10-04 05:08:09 +0000

    deploy

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -1,14 +1,25 @@
+# ===========================
+# Fetch existing CodeDeploy Role
+# ===========================
+data "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role-mayank"
+}
+
+# ===========================
 # CodeDeploy ECS Application
+# ===========================
 resource "aws_codedeploy_app" "strapi_app" {
   name             = "strapi-app-mayank"
   compute_platform = "ECS"
 }
 
+# ===========================
 # CodeDeploy Deployment Group
+# ===========================
 resource "aws_codedeploy_deployment_group" "strapi_deploy_group" {
   app_name              = aws_codedeploy_app.strapi_app.name
   deployment_group_name = "strapi-deploy-group-mayank"
-  service_role_arn      = var.codedeploy_role_arn
+  service_role_arn      = data.aws_iam_role.codedeploy_role.arn
 
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 
@@ -19,7 +30,7 @@ resource "aws_codedeploy_deployment_group" "strapi_deploy_group" {
 
   blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
-      action                         = "TERMINATE"
+      action                          = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
   }
@@ -42,4 +53,32 @@ resource "aws_codedeploy_deployment_group" "strapi_deploy_group" {
       }
     }
   }
+}
+
+# ===========================
+# ECS Service (Blue/Green)
+# ===========================
+resource "aws_ecs_service" "strapi_service" {
+  name            = "strapi-service-mayank"
+  cluster         = aws_ecs_cluster.strapi_cluster.id
+  task_definition = aws_ecs_task_definition.strapi_task.arn
+  desired_count   = 1
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi_tg_blue.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.strapi_listener]
 }
