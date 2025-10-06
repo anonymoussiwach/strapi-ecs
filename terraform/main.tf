82d3d191d05efa3d9836530d98cd4ff4116bdcce
Mayank Siwach <maxsiwach@gmail.com>
2025-10-06 11:10:49 +0000

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service-mayank"
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
-  desired_count   = 1
+  desired_count   = 2
   launch_type     = "FARGATE"
   platform_version = "LATEST"
   
